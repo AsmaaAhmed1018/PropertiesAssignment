1d6bb6ba16ee0fd443e83bd1186139ade5acaad2
asmaaahmed1018 <asmaaahmed1018@gmail.com>
2025-02-14 18:07:47 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,43 @@
+﻿namespace PropertiesAssignment
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; } = 0;
+        public int AccountHolderName { get; set; }
+        public int balance = 0;
+        public int Balance
+        {
+            get
+            {
+                return balance;
+            }
+            private set
+            {
+                if (value > 0) { balance = value; }
+            }
+        }
+        public BankAccount()
+        {
+            AccountNumber++;
+        }
+        public int Deposit(int deposite)
+        {
+            if (deposite > 0)
+            {
+                balance += deposite;
+            }
+            return balance;
+        }
+        public string Withdraw(int deposite)
+        {
+            if (deposite > 0 && balance > deposite)
+            {
+                return $"your new balance is {balance - deposite} ";
+            }
+            else
+            {
+                return "Invalid procees, please enter a valid value";
+            }
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,6 @@
+﻿namespace PropertiesAssignment
+{
+    internal class Product
+    {
+    }
+}
