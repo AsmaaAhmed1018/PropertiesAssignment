d8c221a419a82cfa601eb119a77e8823414aa703
asmaaahmed1018 <asmaaahmed1018@gmail.com>
2025-02-14 18:50:50 +0200

    The first update

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PropertiesAssignment
+{
+    internal class Car
+    {
+        public int Make { get; set; }
+        public int Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; } = 0;
+        public void Drive(int distance)
+        {
+            if (distance > 0)
+            {
+                Mileage += distance;
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = DateTime.Now.Year - Year;
+            }
+        }
+        public Car()
+        {
+            Year = 2012;
+        }
+    }
+}
--- Product.cs
+++ Product.cs
@@ -1,6 +1,56 @@
-﻿namespace PropertiesAssignment
+﻿using System.Security.Cryptography;
+
+namespace PropertiesAssignment
 {
     internal class Product
     {
+        public string Name { get; set; }
+        private int price;
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+            }
+        }
+        private int stockQuantity;
+        public int StockQuantity
+        {
+            get { return stockQuantity; }
+            private set
+            {
+                if (value > 0)
+                {
+                    stockQuantity = value;
+                }
+            }
+        }
+        private bool isAvailable;
+        public bool IsAvailable
+        {
+            get
+            {
+                return isAvailable;
+            }
+            set
+            {
+                if (stockQuantity > 0)
+                {
+                    isAvailable = true;
+                }
+            }
+        }
+        public int UpdateStore(int count)
+        {
+            if (count >0)
+            {
+                stockQuantity += count;
+            }
+            return stockQuantity;
+        }
     }
 }
